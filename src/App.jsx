57df2065cb4bceea80f6d2bf9908f4a8a7cbc2d5
Alec DuBois <after.alec@gmail.com>
2020-09-30 10:12:17 -0700

    compiles j + d + a changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,56 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import StoreFront from "./components/StoreFront";
 import Checkout from "./components/Checkout";
-import OrderConfirmation from "./components/OrderConfirmation"
-import Signup from './components/Signup'
+import OrderConfirmation from "./components/OrderConfirmation";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 export default function App() {
   const [gifs, setGifs] = useState([]);
-  const [orderConf, setOrderConf] = useState(false)
-  // const [cart, setCart] = useState(loadCartFromLocalStorage || []);
-
+  const [orderConf, setOrderConf] = useState(false);
 
   useEffect(() => {
     fetchGifs().then(loadCartFromLocalStorage);
   }, []);
 
   function addGifToCart(id) {
-    const newGifs = gifs.map((gif) => (gif.id === id ? { ...gif, cart: true } : gif))
+    const newGifs = gifs.map((gif) =>
+      gif.id === id ? { ...gif, cart: true } : gif
+    );
     setGifs(newGifs);
-    localStorage.setItem('cart', JSON.stringify({ content: cartGifs(newGifs) }))
+    localStorage.setItem(
+      "cart",
+      JSON.stringify({ content: cartGifs(newGifs) })
+    );
   }
 
   function remGifFromCart(id) {
-    const newGifs = gifs.map((gif) => (gif.id === id ? { ...gif, cart: false } : gif))
+    const newGifs = gifs.map((gif) =>
+      gif.id === id ? { ...gif, cart: false } : gif
+    );
     setGifs(newGifs);
-    localStorage.setItem('cart', JSON.stringify({ content: cartGifs(newGifs) }))
+    localStorage.setItem(
+      "cart",
+      JSON.stringify({ content: cartGifs(newGifs) })
+    );
   }
 
-  function emptyCart(){
-    localStorage.removeItem('cart')
-    setGifs(gifs=> gifs.map(gif => ({...gif, cart: false})))
+  function emptyCart() {
+    localStorage.removeItem("cart");
+    setGifs((gifs) => gifs.map((gif) => ({ ...gif, cart: false })));
   }
 
   function loadCartFromLocalStorage(gifs) {
-    // console.log(gifs)
-    const cart = localStorage.getItem('cart') || false
-    // console.log(cart)
+    const cart = localStorage.getItem("cart") || false;
+
     if (cart) {
-      const cartObjs = JSON.parse(cart).content
-      const cartIds = cartObjs.map(gif => gif.id)
-      const newGifs = gifs.filter(gif => !cartIds.includes(gif.id))
-      setGifs([...newGifs, ...cartObjs])
-    }
-    else {
-      setGifs(gifs)
+      const cartObjs = JSON.parse(cart).content;
+      const cartIds = cartObjs.map((gif) => gif.id);
+      const newGifs = gifs.filter((gif) => !cartIds.includes(gif.id));
+      setGifs([...newGifs, ...cartObjs]);
+    } else {
+      setGifs(gifs);
     }
   }
 
@@ -60,11 +68,12 @@ export default function App() {
           <Cart
             gifs={cartGifs(gifs)}
             handleClick={remGifFromCart}
-            linkTo='/checkout'
-            buttonText='Proceed to Checkout'
+            linkTo="/checkout"
+            buttonText="Proceed to Checkout"
           />
           <StoreFront gifs={gifs} handleClick={addGifToCart} />
         </Route>
+
         <Route path="/checkout">
           <NavBar />
           <Checkout
@@ -74,16 +83,25 @@ export default function App() {
             emptyCart={emptyCart}
           />
         </Route>
+
         <Route path="/thank-you">
           <NavBar />
-          <OrderConfirmation
-            confirmation={orderConf}
-          />
+          <OrderConfirmation confirmation={orderConf} />
         </Route>
+
         <Route path="/signup">
           <NavBar />
-          <Signup
-          />
+          <Signup />
+        </Route>
+
+        <Route path="/login">
+          <NavBar />
+          <Login />
+        </Route>
+
+        <Route path="/profile">
+          <NavBar />
+          <Profile />
         </Route>
       </Switch>
     </Router>
@@ -91,5 +109,5 @@ export default function App() {
 }
 
 function cartGifs(gifs) {
-  return gifs.filter((gif) => gif.cart)
+  return gifs.filter((gif) => gif.cart);
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Profile() {
+  return <div>I am a Profile Page</div>;
+}
