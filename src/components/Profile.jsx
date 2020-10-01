c274a3315118f57425993b9bed23b939f203f531
Alec DuBois <after.alec@gmail.com>
2020-09-30 22:25:44 -0700

    adds load cart handling on log in

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { useHistory } from 'react-router-dom';
 
-export default function Profile({name, email, address, handleDelete}) {
-  
-  const history = useHistory()
-
+export default function Profile({ name, email, address, handleDelete }) {
   return (
     <div>
-      Welcome {name} 
+      Welcome {name}
       <div>{name}</div>
       <div>{email}</div>
       <div>{address}</div>
